bc7213889847ec477bdf8c0621770ecd469d34a9
Liam Thorne <lnthorne@shaw.ca>
2024-06-04 21:27:17 -0700

    remove package init

--- src/controllers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .audio_controller import AudioController
-from .image_controller import ImageController
-from .menu_controller import MenuController
-
-__all__ = ["AudioController", "ImageController", "MenuController"]
--- src/views/__init__.py
+++
@@ -1,5 +0,0 @@
-from .menu_view import MenuView
-from .audio_view import AudioView
-from .image_view import ImageView
-
-__all__ = ["MenuView", "AudioView", "ImageView"]
